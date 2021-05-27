5303e2c2df13af0e56f9fdc750b988527aa899cf
mihanik2828 <79804894+mihanik2828@users.noreply.github.com>
2021-05-27 17:52:59 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,12 +1,87 @@
-unsigned int faStr1(const char *str)
+#include "fun.h"
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+unsigned int faStr1(const char* str)
 {
-    return 0;
+
+    int i = 0, count = 0;
+    bool good;
+    while(str[i] != '\0' )
+    {
+        good =true;
+        if (str[i] != ' ')
+        {
+            while (str[i] != ' ')
+            {
+                if ((str[i] < 58) && (str[i] > 47))
+                {
+                    good = false;
+                    i++;
+                }
+                else i++;
+            }
+            if (good) count++;
+        }
+        else i++;
+    }
+    return count;
 }
-unsigned int faStr2(const char *str)
+
+unsigned int faStr2(const char* str)
 {
-    return 0;
+    int i = 0, count = 0;
+    bool good;
+    while (str[i] != '\0')
+    {
+        good = false;
+        if (str[i] != ' ')
+        {
+            if ((str[i] < 91) && (str[i] > 64))
+            {
+                i++;
+                good = true;
+                while ((str[i] != ' ') && (str[i] != '\0'))
+                {
+                    if ((str[i] > 122) || (str[i] < 95)) good = false;
+                    i++;
+                }
+                if (good) count++;
+            }
+            else i++;
+        }
+        else i++;
+    }
+    return count;
 }
-unsigned int faStr3(const char *str)
+
+
+unsigned int faStr3(const char* str)
 {
-    return 0;
+    bool isWord = false;
+    int i = 0, countWords = 0, countSymbols = 0, helpwhole;
+    bool good; 
+
+    while (str[i] != '\0')
+    {
+        if (str[i] != ' ')
+        {
+            
+            countSymbols++;
+            if ((str[i + 1] == ' ')||(str[i+1]=='\0')) 
+            { 
+                countWords++;
+            }
+            i++;
+        }
+        else i++;
+    }
+    if (countSymbols == 0) return 0;
+    else
+    {
+
+        helpwhole = int((double)countSymbols / countWords + 0.5);
+        return helpwhole; 
+    }
 }
