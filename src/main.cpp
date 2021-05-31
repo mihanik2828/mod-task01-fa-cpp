9b07209d0113cdf685f336c2caa02d9e328a5434
mihanik2828 <79804894+mihanik2828@users.noreply.github.com>
2021-05-31 21:16:09 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,5 @@ int main()
 	std::cout << faStr1("qwe4  gftt e 555gr rrgr  grhh ") << "\n";
 	std::cout << faStr2("Hello, world!") << "\n";
 	std::cout << faStr3(" bad dog ") << "\n";
-	system("pause");
 	return 0;
 }
